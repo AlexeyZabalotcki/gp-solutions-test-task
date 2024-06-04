fffc4d9d8014c05461df039259282e75f9276f0b
AlexeyZabolotsky <alexey.zabolotsky@specific-group.com>
2024-06-04 20:16:37 +0300

    Feature: added docker-compose.yml, Dockerfile and migrate to Postgres

--- src/main/resources/db/changelog/db.changelog-init.sql
+++ src/main/resources/db/changelog/db.changelog-init.sql
@@ -1,6 +1,12 @@
+DROP TABLE IF EXISTS address;
+DROP SEQUENCE IF EXISTS address_seq;
+
+CREATE SEQUENCE address_seq start 1 increment 1;
+
+
 -- Create Address table
 CREATE TABLE address (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     house_number INT,
     street VARCHAR(255),
     city VARCHAR(255),
@@ -8,23 +14,40 @@ CREATE TABLE address (
     post_code VARCHAR(10)
 );
 
+DROP TABLE IF EXISTS contacts;
+DROP SEQUENCE IF EXISTS contacts_seq;
+
+CREATE SEQUENCE contacts_seq start 1 increment 1;
+
 -- Create Contacts table
 CREATE TABLE contacts (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     phone VARCHAR(20),
     email VARCHAR(255)
 );
 
+DROP TABLE IF EXISTS arrival_time;
+DROP SEQUENCE IF EXISTS arrival_time_seq;
+
+CREATE SEQUENCE arrival_time_seq start 1 increment 1;
+
 -- Create ArrivalTime table
 CREATE TABLE arrival_time (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    check_in VARCHAR(5),
-    check_out VARCHAR(5)
+    id BIGSERIAL PRIMARY KEY,
+    check_in time(5),
+    check_out time(5)
+--    USING check_in::time without time zone,
+--    USING check_out::time without time zone;
 );
+
+DROP TABLE IF EXISTS hotels;
+DROP SEQUENCE IF EXISTS hotels_seq;
+
+CREATE SEQUENCE hotels_seq start 1 increment 1;
 
 -- Create Hotels table
 CREATE TABLE hotels (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description TEXT,
     brand VARCHAR(255) NOT NULL,
@@ -36,10 +59,17 @@ CREATE TABLE hotels (
     CONSTRAINT fk_arrival_time FOREIGN KEY (arrival_time_id) REFERENCES arrival_time(id)
 );
 
+DROP TABLE IF EXISTS amenities;
+DROP SEQUENCE IF EXISTS amenities_seq;
+
+CREATE SEQUENCE amenities_seq start 1 increment 1;
+
+-- Create Amenities table with foreign key to hotels
 -- Create Amenities table with foreign key to hotels
 CREATE TABLE amenities (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     hotel_id BIGINT NOT NULL,
     name VARCHAR(255) NOT NULL,
     CONSTRAINT fk_hotel_amenities FOREIGN KEY (hotel_id) REFERENCES hotels(id)
 );
+
